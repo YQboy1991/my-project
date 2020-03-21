75049dd836a7f484d42d9bc3e080539e76f691e0
lbl <lblei91@163.com>
2020-03-21 22:51:47 +0800

    增加主函数测试

---
+++ src/main/java/com/lbl/myproject/demo/MainTest.java
@@ -0,0 +1,18 @@
+package com.lbl.myproject.demo;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @author: BaoLei Li
+ * @date: 2020/3/21
+ **/
+public class MainTest {
+
+    public static void main(String[] args) {
+        List<Integer> lists = Lists.newArrayList(3,6,8,2,5,7);
+        System.out.println(lists);
+
+    }
+}
