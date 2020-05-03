5cb2dc3a7335e314817269b253c7250160ff33e7
YQboy1991 <lblei91@163.com>
2020-05-03 18:53:12 +0800

    Update MyProjectApplication.java
    sadf

--- src/main/java/com/lbl/myproject/MyProjectApplication.java
+++ src/main/java/com/lbl/myproject/MyProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyProjectApplication {
 
     public static void main(String[] args) {
+        // 445
         SpringApplication.run(MyProjectApplication.class, args);
     }
 
