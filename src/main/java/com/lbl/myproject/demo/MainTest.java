b399a726ab849bd5ae2c55f3384eb8a472b329fe
lbl <lblei91@163.com>
2020-05-03 18:36:42 +0800

    新修改

--- src/main/java/com/lbl/myproject/demo/MainTest.java
+++ src/main/java/com/lbl/myproject/demo/MainTest.java
@@ -1,8 +1,17 @@
 package com.lbl.myproject.demo;
 
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.PatternMatchUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: BaoLei Li
@@ -10,9 +19,30 @@ import java.util.List;
  **/
 public class MainTest {
 
+    private static final String[] DEFAULT_REFERER_REGULARS = { "*.kooup.com", "*.koolearn.com", "*.koo.cn",
+            "*.donut.cn", "*.koolearn-inc.com" };
+
     public static void main(String[] args) {
-        List<Integer> lists = Lists.newArrayList(3,6,8,2,5,7);
-        System.out.println(lists);
+//        String origin = "";
+//        String referer = "https://s.neibu.kooup.com/pcapp/mycourses";
+//        try {
+//            URI uri = new URI(referer);
+//            if (PatternMatchUtils.simpleMatch(DEFAULT_REFERER_REGULARS, uri.getHost())) {
+//                StringBuilder originStringBuilder = new StringBuilder(uri.getScheme()).append("://")
+//                        .append(uri.getHost());
+//                if (uri.getPort() > 0) {
+//                    originStringBuilder.append(":").append(uri.getPort());
+//                }
+//                origin = originStringBuilder.toString();
+//            }
+//        }
+//        catch (Throwable t) {
+//        }
+//        System.out.println(origin);
+        Map<Object, Object> objectObjectMap = Collections.emptyMap();
+        objectObjectMap.put(1,1);
+        System.out.println(objectObjectMap);
+
 
     }
 }
