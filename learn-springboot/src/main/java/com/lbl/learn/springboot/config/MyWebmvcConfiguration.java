422b67d7b1711eb39065c8fbaa2c52a0c75145c5
李宝磊 <libaolei@koolearn.com>
2021-09-10 15:52:56 +0800

    feature(conf/code):

--- learn-springboot/src/main/java/com/lbl/learn/springboot/config/MyWebmvcConfiguration.java
+++ learn-springboot/src/main/java/com/lbl/learn/springboot/config/MyWebmvcConfiguration.java
@@ -3,6 +3,7 @@ package com.lbl.learn.springboot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -23,5 +24,10 @@ public class MyWebmvcConfiguration implements WebMvcConfigurer {
     @Bean
     public JsonPHttpMessageConverter jsonPHttpMessageConverter(){
         return new JsonPHttpMessageConverter();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/static/**").addResourceLocations("classpath:/custom/");
     }
 }
---
+++ learn-springboot/src/main/java/com/lbl/learn/springboot/controller/CustomErrorController.java
@@ -0,0 +1,29 @@
+package com.lbl.learn.springboot.controller;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * @author: BaoLei Li
+ * @date: 2021/9/10
+ **/
+@RestController
+@RequestMapping("/api/error")
+public class CustomErrorController {
+
+    @GetMapping("/test")
+    public String testErrorHandler(){
+        throw new RuntimeException();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public Map<String, Object> handlerErrorControllerError() {
+        return ImmutableMap.<String, Object> of("status", 500, "message", "inner error");
+    }
+}
